130d24639e003e9025efd6767e3bf0eb01ce11b2
Frances Dalla Torre <francesdallatorre@MacBook-Pro.local>
2021-02-11 08:33:21 -0500

    added counter screen, on button, and gradient colors

--- project1/js/app.js
+++ project1/js/app.js
@@ -1,24 +1,23 @@
 class Simon {
-    constructor(colors, computer, player, start, level, currentLevel, playerSequence) {
+    constructor(colors, computer, player, start, level, playerSequence) {
         this.colors = ['green', 'red', 'yellow', 'blue'];
         this.computer = [];
         this.player = [];
         this.initiate = false;
         this.level = 0;
-        this.currentLevel = 0;
+
         // this function will take the user's input and save it into playerSequence variable, then call the function checkClick to compare players input with computer's input
         $('.btn').on('click', (event) => {
             console.log(event.currentTarget.id)
             const playerClick = event.currentTarget.id
             this.player.push(playerClick);
             this.checkClick(this.player.length - 1)
-            this.roundManager()
         })
-
     }
     // this function initiates the game by prompting a random color
     init() {
-        $('.start').on('click', () => {
+        $('.start').one('click', () => {
+            $('.start').css('background-color', 'rgb(255, 102, 102)')
             this.nextSequence()
             this.initiate = true;
         })
@@ -27,11 +26,16 @@ class Simon {
     nextSequence() {
         this.player = []
         this.level++
-        $('.start').text('level: ' + this.level)
+        $('.count').text(this.level)
         const randomColor = this.colors[Math.floor(Math.random() * 4)]
         this.computer.push(randomColor)
-        $('#' + randomColor).fadeOut(100).fadeIn(100)
-        // flash and play music
+        setTimeout(() => {
+            $('#' + randomColor).addClass(randomColor + "-flash")
+            // play sounds
+            setTimeout(() => {
+                $('#' + randomColor).removeClass(randomColor + "-flash")
+            }, 500);
+        }, 500);
 
     }
     // this function's control flow verify the equality of playerSequence with gameSequence, if it is equal we fire nextSequence for another round
@@ -41,15 +45,16 @@ class Simon {
             if (this.player.length === this.computer.length) {
                 setTimeout(() => {
                     this.nextSequence();
-                }, 1000)
+                }, 500)
+                this.roundManager()
             }
         } else {
-            $('.start').text('GAME OVER');
             setTimeout(() => {
             }, 1000);
-            $('.start').text("Start")
+            $('.start').css('background-color', 'red')
             this.reStart()
             console.log('game over')
+
         }
     }
     // this function will resets game values
@@ -57,10 +62,14 @@ class Simon {
         this.level = 0;
         this.computer = []
         this.initiate = false;
+        $('.count').text('0');
+        this.init()
     }
     // this function creates rounds 
     roundManager() {
-
+        if (this.level === 5) {
+            console.log("we have a winner")
+        }
     }
 }
 const game = new Simon();
