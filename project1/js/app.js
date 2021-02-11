328899971f4df6513f5eab890b0be2122cac83f3
Frances Dalla Torre <francesdallatorre@MacBook-Pro.local>
2021-02-11 18:24:45 -0500

    added media queries

--- project1/js/app.js
+++ project1/js/app.js
@@ -1,12 +1,11 @@
 class Simon {
-    constructor(colors, computer, player, start, level, playerSequence, speed) {
+    constructor(colors, computer, player, start, level, speed) {
         this.colors = ['green', 'red', 'yellow', 'blue'];
         this.computer = [];
         this.player = [];
         this.initiate = false;
         this.level = 0;
         this.speed;
-
         // this function will take the user's input and save it into playerSequence variable, then call the function checkClick to compare players input with computer's input
         $('.btn').on('click', (event) => {
             console.log(event.currentTarget.id)
@@ -30,7 +29,6 @@ class Simon {
         this.player = []
         this.level++
         this.speed = 1000;
-        this.slowDown = 1000
         $('.count').text(this.level)
         const randomColor = this.colors[Math.floor(Math.random() * 4)]
         this.computer.push(randomColor)
@@ -39,8 +37,8 @@ class Simon {
             // play sounds
             setTimeout(() => {
                 $('#' + randomColor).removeClass(randomColor + "-flash")
-            }, 800);
-        }, 800);
+            }, 1000);
+        }, 1000);
     }
     // this function's control flow verify the equality of playerSequence with gameSequence, if it is equal we fire nextSequence for another round
     checkClick(round) {
@@ -48,22 +46,14 @@ class Simon {
         console.log(round)
         if (this.player[round] === this.computer[round]) {
             if (this.player.length === this.computer.length) {
-                setTimeout(() => {
-                    this.flashSequence()
-                    setTimeout(() => {
-                        this.nextSequence();
-                    }, 500)
-                }, 2000);
+                this.nextSequence()
             } else if (this.level === 7) {
                 $('.game').css('display', 'none')
             }
         } else {
             setTimeout(() => {
             }, 1000);
-            $('.game').css('display', 'none')
-            $('#over').text("Game Over").addClass('over')
             this.reStart()
-
         }
     }
     // this function will resets game values
@@ -76,18 +66,6 @@ class Simon {
             location.reload()
         }, 3000);
     }
-    // this function creates rounds 
-    flashSequence() {
-        for (let i = 0; i < this.computer.length; i++) {
-            setTimeout(() => {
-                $('.' + this.computer[i]).addClass(this.computer[i] + '-flash');
-                setTimeout(() => {
-                    $('.' + this.computer[i]).removeClass(this.computer[i] + '-flash');
-                }, 50);
-            }, 300 * i + 2);
-        }
-    }
-
 }
 const game = new Simon();
 game.init()
